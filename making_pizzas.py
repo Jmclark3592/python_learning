8608978b6389e6980c91fe472eb574e12f154ecd
Jmclark3592 <jmclark3592@gmail.com>
2023-06-20 15:05:25 -0500

    message is needed?

--- making_pizzas.py
+++ making_pizzas.py
@@ -1,11 +1,3 @@
-print('hello world!')
-print('what is your name?')
-myName = input()
 
-print('it is good to meet you, ' + myName)
-print('the length of your name is ' + str(len(myName)))
-print(len(myName))
-
-print('what is your age?')
-age = input()
-print('you will be ' + str(int(age) + 1) + ' in a year')
+for i in range(1, 11):
+    print(i)
--- swordfish.py
+++ swordfish.py
@@ -1,12 +1,6 @@
-#password swordfish
 
+count = 0
 while True:
-    name = input("what's your name? ")
-    if name.lower() != 'justin':
-        continue
-    password = input('what is the password? ')
-    if password.lower() != "swordfish":
-        continue
-    else:
-        break
-print("access granted")
+    if count < 11:
+        print(count)
+    count = count + 1
