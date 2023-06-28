7fa415edd6ee535f4ed66a9335f821b9df708166
Jmclark3592 <jmclark3592@gmail.com>
2023-06-28 15:04:18 -0500

    updated game with helpful comments

--- arena.py
+++ arena.py
@@ -4,13 +4,10 @@ print('''\tWelcome to the arena''')
 
 npc1 = {'str1' : 8, 'agi' : 6, 'hp' : 28, 'alive' : 1}
 npc2 = {'str1' : 4, 'agi' : 9, 'hp' : 32, 'alive' : 1}
-npc3 = {'str1' : 6, 'agi' : 7, 'hp' : 40, 'alive' : 1}
-npc4 = {'str1' : 8, 'agi' : 8, 'hp' : 40, 'alive' : 1}
 
 player1 = {'str1' : 10, 'agi' : 8, 'hp' : 40}
 
-
-def comp_attack(char_stats):
+def comp_attack(char_stats): #function for computer to attack. Each attack adjusts for strength factor (str1)
     attack = random.choice(range(11))
     adjustment = int((char_stats * 0.2) * attack)
     if attack == 0:
@@ -19,7 +16,7 @@ def comp_attack(char_stats):
         print(f'NPC hits player for {adjustment} HP')
     player1['hp'] = player1['hp'] - adjustment
 
-def player_attack(char_stats, npc):
+def player_attack(char_stats, npc): #function for player to attack
     attack = random.choice(range(11))
     adjustment = int((char_stats * 0.2) * attack)
     if attack == 0:
@@ -41,7 +38,7 @@ while True:
         print("You have died!")
         break
     
-    if npc1['alive'] == 1:
+    if npc1['alive'] == 1: #if NPC1 is alive, they check AGI to see who goes first and then battle
         agi_check(npc1['agi'], player1['agi'])
         if flag == False:
             comp_attack(npc1['str1'])
@@ -50,26 +47,26 @@ while True:
             print(f"Computer HP has dropped to {npc1['hp']}")
             if npc1['hp'] <= 0:
                 print("You beat NPC1!")
-                npc1['alive'] = 0
+                npc1['alive'] = 0 #if NPC1 hp goes to 0 flag sets to False and runs back loop down to NPC2
                 print("\nNPC2 has entered the arena")
             if player1['hp'] <= 0:
                 print("You have died!")
                 break
-        else:
+        else: #if player1 AGI is higher then he goes first
             player_attack(player1['str1'], npc1)
             print(f"Computer HP has dropped to {npc1['hp']}") 
             comp_attack(npc1['str1'])
             print(f"Player HP has dropped to {player1['hp']}")
             if npc1['hp'] <= 0:
                 print("You beat NPC1!")
-                npc1['alive'] = 0
+                npc1['alive'] = 0 #if NPC1 hp goes to 0 flag sets to False and runs back loop down to NPC2
                 print("\nNPC2 has entered the arena")
             if player1['hp'] <= 0:
                 print("You have died!")
                 break
 
 
-    elif npc2['alive'] == 1:
+    elif npc2['alive'] == 1: #After NPC1 dies, player1 hp is reset and we do AGI check to see who goes first for this fight
         player1['hp'] = 40
         agi_check(npc2['agi'], player1['agi'])
         if flag == False:
@@ -80,6 +77,7 @@ while True:
             if npc2['hp'] <= 0:
                 print("You beat NPC1!")
                 npc2['alive'] = 0
+                break 
             if player1['hp'] <= 0:
                 print("You have died!")
                 break
@@ -92,6 +90,7 @@ while True:
                 print("You beat NPC1!")
                 npc2['alive'] = 0
                 print("\nNPC3 has entered the arena")
+                break
             if player1['hp'] <= 0:
                 print("You have died!")
                 break
