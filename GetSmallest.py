10c4bc94ecf9856d6f3b82c3e71366819f2b32ab
Jmclark3592 <jmclark3592@gmail.com>
2023-07-10 16:25:37 -0500

    fixed getsmallest

--- GetSmallest.py
+++ GetSmallest.py
@@ -1,15 +1,11 @@
 def getSmallest(numbers):
-    for num in numbers:
-        smallest = sorted(numbers)
-        return smallest
+    if numbers == []:
+        return None
+    return min(numbers)
 
 
-numb = [8, 4, 8, 99, 12]
-numb1 = getSmallest(numb)
-print(numb1[0])
-
-# assert getSmallest([1, 2, 3]) == 1
-# assert getSmallest([3, 2, 1]) == 1
-# assert getSmallest([28, 25, 42, 2, 28]) == 2
-# assert getSmallest([1]) == 1
-# assert getSmallest([]) == None
+assert getSmallest([1, 2, 3]) == 1
+assert getSmallest([3, 2, 1]) == 1
+assert getSmallest([28, 25, 42, 2, 28]) == 2
+assert getSmallest([1]) == 1
+assert getSmallest([]) == None
