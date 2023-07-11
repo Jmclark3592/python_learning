07154300398c05a1a1e88d548de64556fe882dae
Jmclark3592 <jmclark3592@gmail.com>
2023-07-11 13:36:22 -0500

    av calc

---
+++ AvgCalc.py
@@ -0,0 +1,19 @@
+def average(numbers):
+    var = sum(numbers)
+    avg = var / len(numbers)
+    return int(avg)
+
+
+print(average([1, 2, 3]))
+
+assert average([1, 2, 3]) == 2
+assert average([1, 2, 3, 1, 2, 3, 1, 2, 3]) == 2
+assert average([12, 20, 37]) == 23
+assert average([0, 0, 0, 0, 0]) == 0
+import random
+
+random.seed(42)
+testData = [1, 2, 3, 1, 2, 3, 1, 2, 3]
+for i in range(1000):
+    random.shuffle(testData)
+    assert average(testData) == 2
